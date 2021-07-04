fcaad2c8a4bfb42f5eb641d6b2c7ea5f52a7b1e5
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-07-04 13:38:20 +0500

    Added frontend-deploy stack and its tests

--- backend/lib/p14d-frontend-deploy-stack.ts
+++ backend/lib/p14d-frontend-deploy-stack.ts
@@ -1,9 +1,41 @@
 import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 export class FrontendDeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketForFrontendAssets = new s3.Bucket(
+      this,
+      "P14dBucketForFrontendAssets",
+      {
+        bucketName: "p14d-bucket-for-frontend-assets",
+        versioned: true,
+        autoDeleteObjects: true,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        websiteIndexDocument: "index.html",
+        publicReadAccess: true,
+      }
+    );
+
+    const distribution = new cloudfront.Distribution(this, "P14dFrontendDist", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucketForFrontendAssets),
+      },
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responsePagePath: "/404.html",
+        },
+      ],
+    });
+
+    new cdk.CfnOutput(this, "CloudFrontUrl", {
+      value: distribution.domainName,
+    });
+
+    cdk.Tags.of(this).add("Project", "P14d-Pet-Theory-Event-Driven");
   }
 }
--- backend/test/p14d-frontend-deploy-stack.test.ts
+++ backend/test/p14d-frontend-deploy-stack.test.ts
@@ -1,25 +1,22 @@
-import {
-  expect as expectCDK,
-  matchTemplate,
-  MatchStyle,
-} from "@aws-cdk/assert";
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
 import { FrontendDeployStack } from "../lib/p14d-frontend-deploy-stack";
 
 const createTestStack = (app: cdk.App) =>
   new FrontendDeployStack(app, "MyTestStack");
 
-test("Empty Stack", () => {
+test("Stack has a S3 Bucket", () => {
   const app = new cdk.App();
   // WHEN
   const stack = createTestStack(app);
   // THEN
-  expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
-      },
-      MatchStyle.EXACT
-    )
-  );
+  expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+});
+
+test("Stack has a CloudFront Distribution", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = createTestStack(app);
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::CloudFront::Distribution"));
 });
